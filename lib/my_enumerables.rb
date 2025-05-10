7367b080c7cbbc89ece04e4dd19215fd3a1de7d6
Romain Gremaud <romaingremaud@mac.attlocal.net>
2025-05-09 22:09:32 -0400

    Add methods for all but each with index and injext

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,13 +1,57 @@
 module Enumerable
-  # Your code goes here
+  def my_all? # doesn't work - same as any?
+    self.my_each do |elem|
+      return false unless yield(elem)
+    end
+    true
+  end
+
+  def my_none? 
+    self.my_each do |elem|
+      return false if yield(elem)
+    end
+    true
+  end
+
+  def my_any?
+    self.my_each do |elem|
+      return true if yield(elem)
+    end
+    false
+  end
+
+  def my_map
+    new_array = []
+    for item in self
+      new_array << yield(item)
+    end
+    new_array
+  end
+
+  def my_select
+    new_array = []
+    for item in self
+      if yield(item) == true
+        new_array << item
+      end
+    end
+    new_array
+  end
+  
+  def my_count # doesn't work
+    count = 0
+    new_array = []
+    for item in self
+      if yield(item)
+        new_array << item
+      end
+    end
+    count = new_array.length
+  end
+
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
   def my_each
     for item in self
       yield(item)
